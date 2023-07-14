6ebf7cd56a8da36d9902c41f49e20a219c1bf0ef
River <hom0906@naver.com>
2023-07-14 18:27:07 +0900

    Player Jump

--- Player.cs
+++ Player.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float jumpPower;
 
     bool wDown;
+    bool jDown;
+
+    bool isJump;
+    bool isDodge;
 
     float hAxis;
     float vAxis;
+
     Vector3 moveVec;
+    Vector3 dodgeVec;
 
     Animator anim;
+    Rigidbody rigid;
 
     void Awake()
     {
         anim = GetComponentInChildren<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -24,6 +33,8 @@ public class Player : MonoBehaviour
         InputSystem();
         Move();
         Turn();
+        Jump();
+        Dodge();
     }
 
     void InputSystem()
@@ -31,11 +42,14 @@ public class Player : MonoBehaviour
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
+        jDown = Input.GetButtonDown("Jump");
     }
 
     void Move()
     {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        if(isDodge) moveVec = dodgeVec;
 
         transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
 
@@ -46,5 +60,49 @@ public class Player : MonoBehaviour
     void Turn()
     {
         transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if(jDown && !isJump && !isDodge && moveVec == Vector3.zero)
+        {
+            isJump = true;
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+        }
+    }
+
+    void Dodge()
+    {
+        if(jDown && !isJump && !isDodge && moveVec != Vector3.zero)
+        {
+            isDodge = true;
+            StartCoroutine(DodgeRoutine());
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+        }
+    }
+
+    IEnumerator DodgeRoutine()
+    {
+        speed *= 4;
+        isDodge = true;
+        dodgeVec = moveVec;
+        anim.SetTrigger("doDodge");
+
+        yield return new WaitForSeconds(0.35f);
+
+        isDodge = false;
+        speed *= 0.25f;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Floor")
+        {
+            isJump = false;
+            anim.SetBool("isJump", false);
+        }
     }
 }
